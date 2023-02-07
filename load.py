bdafc539683399810f1e3212f8c8005675335269
Chelsey Shao <90055526+SSH-C12138@users.noreply.github.com>
2023-02-07 13:06:20 +0000

    update load.py and add run_eva

--- load.py
+++ load.py
@@ -1,4 +1,9 @@
-import os, yaml
+import sys, os, datetime, glob, time, math, configparser, json, yaml
+sys.path.append(f"{os.path.dirname(os.getcwd())}/cr_monitor")
+from bokeh.io import output_notebook
+output_notebook()
+import pandas as pd
+import numpy as np
 with open(f"{os.environ['HOME']}/.cryptobridge/private_key.yml", "rb") as f:
     data = yaml.load(f, Loader= yaml.SafeLoader)
 for info in data:
