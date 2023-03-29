6bc9367da82a714f719c241fdf212ee64a59d592
Chelsey Shao <90055526+SSH-C12138@users.noreply.github.com>
2023-03-29 10:10:00 +0000

    update bft2.0 config

--- cr_assis/run/run.sh
+++ cr_assis/run/run.sh
@@ -1,1 +1,1 @@
-/opt/anacaonda3/bin/python3 "/home/ssh/jupyter/cr_assis/run_buffet2.py"
+/opt/anacaonda3/bin/python3 "/home/ssh/jupyter/cr_assis/cr_assis/run/run_buffet2.py"
--- cr_assis/run/run_buffet2.py
+++ cr_assis/run/run_buffet2.py
@@ -2,6 +2,7 @@ from cr_assis.account.accountBase import AccountBase
 from cr_assis.run.buffet2 import Get_Parameter as buffet
 from cr_assis.load import *
 bg003 = AccountBase(deploy_id= "bg_bg003@dt_okex_cswap_okex_uswap_btc", is_usdc= True)
-bft=buffet(accounts = [bg003])
-all_parameter = bft.get_parameter(com=bg003.combo,accounts=[],ingore_account=[],upload= False)
+ht001 = AccountBase(deploy_id= "ht_ht001@dt_okex_cswap_okex_uswap_btc", is_usdc= True)
+bft=buffet(accounts = [bg003, ht001])
+all_parameter = bft.get_parameter(com=bg003.combo,accounts=[],ingore_account=[],upload= True)
 all_parameter
