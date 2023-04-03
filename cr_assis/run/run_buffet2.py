6e0c120e9723038fda01069a74453068a6cf71ff
Chelsey Shao <90055526+SSH-C12138@users.noreply.github.com>
2023-04-03 08:24:58 +0000

    update bft2.0 config

--- cr_assis/run/run_buffet2.py
+++ cr_assis/run/run_buffet2.py
@@ -4,6 +4,7 @@ from cr_assis.load import *
 bg003 = AccountBase(deploy_id= "bg_bg003@dt_okex_cswap_okex_uswap_btc", is_usdc= True)
 ht001 = AccountBase(deploy_id= "ht_ht001@dt_okex_cswap_okex_uswap_btc", is_usdc= True)
 anta001 = AccountBase(deploy_id= "anta_anta001@dt_okex_cswap_okex_uswap_btc", is_usdc= True)
-bft=buffet(accounts = [bg003, ht001, anta001])
+ch006 = AccountBase(deploy_id= "ch_ch006@dt_okex_cswap_okex_uswap_btc", is_usdc= True)
+bft=buffet(accounts = [anta001])
 all_parameter = bft.get_parameter(com=bg003.combo,accounts=[],ingore_account=[],upload= True)
 all_parameter
