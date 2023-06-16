30cbde526e1d6b81482dc1ca0d27e2eb6aaecd31
Chelsey Shao <90055526+SSH-C12138@users.noreply.github.com>
2023-06-16 10:43:20 +0000

    add run_gateWallet.pyu

--- cr_assis/run/run_wallet.sh
+++ cr_assis/run/run_wallet.sh
@@ -1,2 +1,3 @@
 /opt/anacaonda3/bin/python3 "/home/ssh/jupyter/cr_assis/cr_assis/run/run_updateGateWallet.py"
 /opt/anacaonda3/bin/python3 "/home/ssh/jupyter/cr_assis/cr_assis/run/run_okexWallet.py"
+/opt/anacaonda3/bin/python3 "/home/ssh/jupyter/cr_assis/cr_assis/run/run_gateWallet.py"
