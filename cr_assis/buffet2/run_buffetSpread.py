16922844d78bdcc48094f5dcd7ffe3c9b29af36c
Chelsey Shao <90055526+SSH-C12138@users.noreply.github.com>
2023-06-20 06:29:02 +0000

    modify accountOkex.exposure_number and setup

--- cr_assis/buffet2/run_buffetSpread.py
+++ cr_assis/buffet2/run_buffetSpread.py
@@ -2,5 +2,5 @@ from cr_assis.buffet2.buffetOkexSpread import BuffetOkexSpread
 from cr_assis.load import *
 
 bft = BuffetOkexSpread()
-bft.json_path = "/Users/chelseyshao/Documents/GitHub/cr_assis/cr_assis/config/buffet2_config/pt"
+bft.json_path = "/home/ssh/jupyter/cr_assis/cr_assis/config/buffet2_config/pt"
 bft.run_buffet(is_save = True, upload= True)
