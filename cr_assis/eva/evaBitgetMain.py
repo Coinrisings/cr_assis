f3a56f5602bfe1c8c95befa69a2b461d6ec588b1
Chelsey Shao <90055526+SSH-C12138@users.noreply.github.com>
2023-06-21 08:07:19 +0000

    add run_updateGitGetMain.py and modify spread config

--- cr_assis/eva/evaBitgetMain.py
+++ cr_assis/eva/evaBitgetMain.py
@@ -3,4 +3,4 @@ from cr_assis.eva.evaGateWallet import EvaGateWallet
 class EvaBitGetMain(EvaGateWallet):
     def __init__(self):
         super().__init__()
-        self.file_path = "/mnt/efs/fs1/data_ssh/mm/capital/gate"
+        self.file_path = "/mnt/efs/fs1/data_ssh/mm/capital/bitget"
