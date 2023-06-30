92f24438ba67a95c0b3f403c44190937a4ec02e5
Chelsey Shao <luc@ip-10-1-1-26.ap-northeast-1.compute.internal>
2023-06-30 11:42:25 +0000

    fix bug about draw_tabs and add kline

--- cr_assis/eva/evaGateWallet.py
+++ cr_assis/eva/evaGateWallet.py
@@ -16,23 +16,28 @@ class EvaGateWallet(object):
         self.max_mv = 0.2
         self.title_name = {"total_pnl": "所有子账户累计交易盈亏总额", "total_capital": "所有子账户子账户资金加总", "total_equity": "所有子账户净值","dnw_sum": "所有子账户累计转入-累计转出", "total_mv": "头寸大小统计", "open": "开盘价", "close": "收盘价"}
     
-    def get_btc_price(self, start: datetime.datetime, end: datetime.datetime) -> pd.DataFrame:
+    def get_btc_price(self, start: datetime.datetime, end: datetime.datetime, bar = "1H") -> pd.DataFrame:
         ts = int(datetime.datetime.timestamp(end)) * 1000
         start_ts = int(datetime.datetime.timestamp(start)) * 1000
         data = []
         while ts >= start_ts:
-            url = f"https://www.okx.com/api/v5/market/candles?instId=BTC-USDT&bar=1D&limit=300&after={ts}"
+            url = f"https://www.okx.com/api/v5/market/candles?instId=BTC-USDT&bar={bar}&limit=300&after={ts}"
             response = requests.get(url)
             if response.status_code == 429:
                 time.sleep(1)
             elif response.status_code == 200:
                 data += response.json()["data"]
-                ts = int(response.json()["data"][-1][0])
+                if len(response.json()["data"]) > 0:
+                    ts = int(response.json()["data"][-1][0])
+                else:
+                    break
             else:
                 print(response.json())
                 break
         kline = pd.DataFrame(data, columns = ["time", "open", "high", "low", "close", "vol", "colCcy", "volCcyQuote", "confirm"])
         kline["dt"] = kline["time"].apply(lambda x: datetime.datetime.fromtimestamp(float(x)/1000).astimezone(pytz.timezone("Asia/ShangHai")))
+        kline["dt"] = kline["dt"].apply(lambda x: x.replace(tzinfo=None))
+        kline = kline[(kline["dt"] >= datetime.datetime.strptime(str(start)[:19].replace("T", " "), "%Y-%m-%d %H:%M:%S")) & (kline["dt"] <= datetime.datetime.strptime(str(end)[:19].replace("T", " "), "%Y-%m-%d %H:%M:%S"))].copy()
         return kline
     
     def read_total_summary(self, start: datetime.datetime, end: datetime.datetime, is_play = True):
@@ -56,18 +61,21 @@ class EvaGateWallet(object):
     
     def draw_result_tabs(self, result: pd.DataFrame):
         tabs = []
+        result.sort_index(inplace = True)
         for col in result.columns:
-            if col not in ["total_equity"]:
-                p = draw_ssh.line(result[[col]], play = False)
-                p.yaxis[0].formatter = NumeralTickFormatter(format="0,0") if col != "total_mv" else NumeralTickFormatter(format="0.0000%")
-            else:
-                kline = self.get_btc_price(result.index[0], result.index[0])
-                kline["dt"] = kline["dt"].apply(lambda x: x.replace(tzinfo=None))
+            if (col == "total_equity" and self.__class__.__name__ == "EvaBitGetMain") or (col == "total_capital" and self.__class__.__name__ == "EvaGateWallet"):
+                kline = self.get_btc_price(result.index[0], result.index[-1])
                 kline.set_index("dt", inplace=True)
-                data = pd.merge(result[[col]], kline["open"], left_index=True, right_index=True, how="outer")
+                kline["open"] = kline["open"].astype(float)
+                data = pd.merge(result[[col]], kline[["open"]], left_index=True, right_index=True, how="outer")
+                data.fillna(method = "ffill", inplace = True)
+                self.data = data
                 p = draw_ssh.line_doubleY(data, right_columns=["open"], play = False)
                 p.yaxis[0].formatter = NumeralTickFormatter(format="0,0") if col != "total_mv" else NumeralTickFormatter(format="0.0000%")
                 p.yaxis[1].formatter = NumeralTickFormatter(format="0.00")
+            else :
+                p = draw_ssh.line(result[[col]], play = False)
+                p.yaxis[0].formatter = NumeralTickFormatter(format="0,0") if col != "total_mv" else NumeralTickFormatter(format="0.0000%")
             tab = Panel(child = p, title = self.title_name[col])
             tabs.append(tab)
         t = Tabs(tabs = tabs)
