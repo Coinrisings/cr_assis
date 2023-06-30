673d4ca680e73d5c68b316ec7bf36871fe11c658
Chelsey Shao <ssh21927@gmail.com>
2023-06-30 17:56:06 +0800

    add btc price in bitget

--- cr_assis/eva/evaBitgetMain.py
+++ cr_assis/eva/evaBitgetMain.py
@@ -1,6 +1,15 @@
+import datetime
 from cr_assis.eva.evaGateWallet import EvaGateWallet
+from cr_assis.load import *
+from cr_assis.load import datetime
 
 class EvaBitGetMain(EvaGateWallet):
     def __init__(self):
         super().__init__()
         self.file_path = "/mnt/efs/fs1/data_ssh/mm/capital/bitget"
+    
+    def read_total_summary(self, start: datetime, end: datetime, is_play=True):
+        super().read_total_summary(start, end, is_play = False)
+        kline = self.get_btc_price(start, end)
+        self.total_summary = pd.merge(self.total_summary, kline.set_index("dt")[["close"]], left_index= True ,right_index= True, how="outer")
+        self.draw_result_tabs(self.total_summary) if is_play else None
