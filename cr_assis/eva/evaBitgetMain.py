a31b23a04c758c112c1a969e1d5bffec89e867f6
Chelsey Shao <luc@ip-10-1-1-26.ap-northeast-1.compute.internal>
2023-06-30 10:30:37 +0000

    update evaBitget

--- cr_assis/eva/evaBitgetMain.py
+++ cr_assis/eva/evaBitgetMain.py
@@ -8,8 +8,3 @@ class EvaBitGetMain(EvaGateWallet):
         super().__init__()
         self.file_path = "/mnt/efs/fs1/data_ssh/mm/capital/bitget"
     
-    def read_total_summary(self, start: datetime, end: datetime, is_play=True):
-        super().read_total_summary(start, end, is_play = False)
-        kline = self.get_btc_price(start, end).set_index("dt")
-        self.total_summary = pd.merge(self.total_summary, kline[["close"]], left_index= True ,right_index= True, how="outer")
-        self.draw_result_tabs(self.total_summary) if is_play else None
