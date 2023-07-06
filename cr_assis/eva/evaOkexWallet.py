dc35802090b76754dfa25ee834bead31e4fedece
Chelsey Shao <luc@ip-10-1-1-26.ap-northeast-1.compute.internal>
2023-07-06 07:16:08 +0000

    update draw_ssh

--- cr_assis/eva/evaOkexWallet.py
+++ cr_assis/eva/evaOkexWallet.py
@@ -20,23 +20,20 @@ class EvaOkexWallet(EvaGateWallet):
     def read_total_summary(self, start: datetime.datetime, end: datetime.datetime, accounts = [],is_play = True):
         accounts = self.accounts if accounts == [] else accounts
         tabs = []
-        accounts_panle = {}
         self.origin_total = {}
         for account in accounts:
             total_summary = self.read_data(path = f"{self.file_path}/{account}", start = start, end = end)
             self.total_summary = total_summary.drop("position_value", axis = 1) if "position_value" in total_summary.columns else total_summary
-            accounts_panle[account] = draw_ssh.line_doubleY(self.total_summary, right_columns=["mv%"], play = False) if is_play and len(self.total_summary) > 0 else None
             kline = self.get_btc_price(start, end)
             kline.set_index("dt", inplace = True)
-            accounts_panle[account].extra_y_ranges['y3'] = Range1d(start = min(kline["open"].astype(float).values), end = max(kline["open"].astype(float).values))
-            accounts_panle[account].add_layout(LinearAxis(y_range_name = 'y3'),'right')
-            accounts_panle[account].line(kline.index, kline["open"], legend_label="kline", line_color="green",name = "kline", y_range_name='y3', line_width = 2)
-            self.origin_total[account] = pd.merge(self.total_summary, kline, left_index=True, right_index=True, how = "outer")
-            if accounts_panle[account] != None:
-                accounts_panle[account].yaxis[0].formatter = NumeralTickFormatter(format="0,0")
-                accounts_panle[account].yaxis[1].formatter = NumeralTickFormatter(format="0.0000%")
-                accounts_panle[account].yaxis[2].formatter = NumeralTickFormatter(format="0.00")
-                tab = Panel(child = accounts_panle[account], title = account)
+            self.origin_total[account] = pd.merge(self.total_summary, kline[["open"]].astype(float), left_index=True, right_index=True, how = "outer").fillna(method = "ffill")
+            if is_play:
+                p = draw_ssh.line_triY(self.origin_total[account],left_columns = ["equity"], play = False)
+                p.yaxis[0].formatter = NumeralTickFormatter(format="0,0")
+                p.yaxis[1].formatter = NumeralTickFormatter(format="0.0000%")
+                p.yaxis[2].formatter = NumeralTickFormatter(format="0.00")
+            if p:
+                tab = Panel(child = p, title = account)
                 tabs.append(tab)
         if len(tabs) > 0:
             t = Tabs(tabs = tabs)
