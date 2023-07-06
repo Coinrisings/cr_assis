78a240b783f113723a421a5658e3d143b2932b13
Chelsey Shao <ssh21927@gmail.com>
2023-07-06 13:47:15 +0800

    add dict to panle

--- cr_assis/eva/evaOkexWallet.py
+++ cr_assis/eva/evaOkexWallet.py
@@ -20,20 +20,21 @@ class EvaOkexWallet(EvaGateWallet):
     def read_total_summary(self, start: datetime.datetime, end: datetime.datetime, accounts = [],is_play = True):
         accounts = self.accounts if accounts == [] else accounts
         tabs = []
+        accounts_panle = {}
         for account in accounts:
             total_summary = self.read_data(path = f"{self.file_path}/{account}", start = start, end = end)
             self.total_summary = total_summary.drop("position_value", axis = 1) if "position_value" in total_summary.columns else total_summary
-            p = draw_ssh.line_doubleY(self.total_summary, right_columns=["mv%"], play = False) if is_play and len(self.total_summary) > 0 else None
+            accounts_panle[account] = draw_ssh.line_doubleY(self.total_summary, right_columns=["mv%"], play = False) if is_play and len(self.total_summary) > 0 else None
             kline = self.get_btc_price(start, end)
             kline.set_index("dt", inplace = True)
-            p.extra_y_ranges['y3'] = Range1d(start = min(kline["open"].astype(float).values), end = max(kline["open"].astype(float).values))
-            p.add_layout(LinearAxis(y_range_name = 'y3'),'right')
-            p.line(kline.index, kline["open"], legend_label="kline", line_color="green",name = "kline", y_range_name='y3', line_width = 2)
-            if p != None:
-                p.yaxis[0].formatter = NumeralTickFormatter(format="0,0")
-                p.yaxis[1].formatter = NumeralTickFormatter(format="0.0000%")
-                p.yaxis[2].formatter = NumeralTickFormatter(format="0.00")
-                tab = Panel(child = copy.deepcopy(p), title = account)
+            accounts_panle[account].extra_y_ranges['y3'] = Range1d(start = min(kline["open"].astype(float).values), end = max(kline["open"].astype(float).values))
+            accounts_panle[account].add_layout(LinearAxis(y_range_name = 'y3'),'right')
+            accounts_panle[account].line(kline.index, kline["open"], legend_label="kline", line_color="green",name = "kline", y_range_name='y3', line_width = 2)
+            if accounts_panle[account] != None:
+                accounts_panle[account].yaxis[0].formatter = NumeralTickFormatter(format="0,0")
+                accounts_panle[account].yaxis[1].formatter = NumeralTickFormatter(format="0.0000%")
+                accounts_panle[account].yaxis[2].formatter = NumeralTickFormatter(format="0.00")
+                tab = Panel(child = accounts_panle[account], title = account)
                 tabs.append(tab)
         if len(tabs) > 0:
             t = Tabs(tabs = tabs)
